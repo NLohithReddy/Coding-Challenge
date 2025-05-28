dbc0ab128755c08b0ea20fc53126201e7ad23300
N Lohith Reddy <114926616+NLohithReddy@users.noreply.github.com>
2025-05-28 13:35:38 +0530

    Update PlayerServiceImpl.java

--- cricketteammanagement/src/main/java/com/hexaware/service/PlayerServiceImpl.java
+++ cricketteammanagement/src/main/java/com/hexaware/service/PlayerServiceImpl.java
@@ -2,9 +2,9 @@ package com.hexaware.service;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service; 
+import org.springframework.stereotype.Service;
+
 import com.hexaware.entity.Player;
 import com.hexaware.exception.PlayerNotFoundException;
 import com.hexaware.repository.PlayerRepository;
@@ -15,25 +15,39 @@ public class PlayerServiceImpl implements PlayerService {
     @Autowired
     private PlayerRepository repo;
 
+    @Override
     public List<Player> getAllPlayers() {
         return repo.findAll();
     }
 
+    @Override
     public Player getPlayerById(Long id) {
         return repo.findById(id)
-                   .orElseThrow(() -> new PlayerNotFoundException("Player not found with id: " + id));
+                .orElseThrow(() -> new PlayerNotFoundException("Player not found with id: " + id));
     }
 
+    @Override
     public Player createPlayer(Player player) {
         return repo.save(player);
     }
 
+    @Override
     public Player updatePlayer(Long id, Player updatedPlayer) {
         Player existing = getPlayerById(id);
-        BeanUtils.copyProperties(updatedPlayer, existing, "playerId");
+
+        
+        existing.setPlayerName(updatedPlayer.getPlayerName());
+        existing.setJerseyNumber(updatedPlayer.getJerseyNumber());
+        existing.setRole(updatedPlayer.getRole());
+        existing.setTotalMatches(updatedPlayer.getTotalMatches());
+        existing.setTeamName(updatedPlayer.getTeamName());
+        existing.setCountryOrState(updatedPlayer.getCountryOrState());
+        existing.setDescription(updatedPlayer.getDescription());
+
         return repo.save(existing);
     }
 
+    @Override
     public void deletePlayer(Long id) {
         repo.deleteById(id);
     }
