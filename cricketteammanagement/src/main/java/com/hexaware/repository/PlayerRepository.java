5b229d100232a7f14e0537b211eb23a838599e90
N Lohith Reddy <114926616+NLohithReddy@users.noreply.github.com>
2025-05-28 17:44:10 +0530

    Delete cricketteammanagement directory

--- cricketteammanagement/src/main/java/com/hexaware/repository/PlayerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hexaware.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.hexaware.entity.Player;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
